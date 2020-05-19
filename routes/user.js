75a4656f6aba11eefbe57b7ae80c3b0552243851
Jai Parmar <jaiparmar207@gmail.com>
2020-05-19 11:56:59 +0530

    new routes for Admin-Faculty-Student added

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
 //login
 router.get('/login',(req,res)=>res.render('/login.ejs'));
 //about
 router.get('/about',(req,res)=>res.send('Welcome to about page'));
-
 
 module.exports = router;
